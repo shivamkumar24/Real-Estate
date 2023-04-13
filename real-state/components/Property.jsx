44e5e322d19753a043bc5db5bf303eb03f994862
Shivam <shivam2432000@gmail.com>
2023-04-14 00:26:10 +0530

    This is some chnages in property components

--- real-state/components/Property.jsx
+++ real-state/components/Property.jsx
@@ -6,8 +6,63 @@ import { BsGridFill } from "react-icons/bs";
 import { GoVerified } from "react-icons/go";
 import millify from "millify";
 
-const Property = () => {
-  return <div>Property Details</div>;
-};
+const Property = ({
+  property: {
+    coverPhoto,
+    price,
+    rentFrequency,
+    rooms,
+    title,
+    baths,
+    area,
+    agency,
+    isVerified,
+    externalId,
+  },
+}) => (
+  <Link href={`/property/${externalId}`} passHref>
+    <Flex
+      flexWrap="wrap"
+      w="420px"
+      p="5"
+      paddingTop="0"
+      justifyContent="flex-start"
+      cursor="pointer"
+    >
+      <Box>
+        <Image width={400} height={250} src={coverPhoto.url} alt="house" />
+      </Box>
+      <Box w="full">
+        <Flex paddingTop="2" alignItems="center" justifyContent="space-between">
+          <Flex alignItems="center">
+            <Box paddingRight="3" color="green.400">
+              {isVerified && <GoVerified />}
+            </Box>
+            <Text fontWeight="bold" fontSize="lg">
+              AED {price}
+              {rentFrequency && `/${rentFrequency}`}
+            </Text>
+          </Flex>
+          <Box>
+            <Avatar size="sm" src={agency?.logo?.url}></Avatar>
+          </Box>
+        </Flex>
+        <Flex
+          alignItems="center"
+          p="1"
+          justifyContent="space-between"
+          w="250px"
+          color="blue.400"
+        >
+          {rooms}
+          <FaBed /> | {baths} <FaBath /> | {millify(area)} sqft <BsGridFill />
+        </Flex>
+        <Text fontSize="lg">
+          {title.length > 30 ? title.substring(0, 30) + "..." : title}
+        </Text>
+      </Box>
+    </Flex>
+  </Link>
+);
 
 export default Property;
